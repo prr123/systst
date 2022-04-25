82e112db68e11222a6bfc49696ffc7fa18661096
prr123 <priemen@gmail.com>
2022-04-25 16:40:56 +0200

    initial commit

---
+++ socket/study-socket1.go
@@ -0,0 +1,42 @@
+package main
+
+// This is a super-quick example of how to set the socket options to allow port re-use for a single address/port on a host machine.
+// This is most commonly used with things like hot reloads of configuration.
+
+import (
+	"context"
+	"log"
+	"net"
+	"syscall"
+	"golang.org/x/sys/unix"
+)
+
+func main() {
+	const network = "tcp"
+	const address = "127.0.0.1:8080"
+
+	config := &net.ListenConfig{Control: reusePort}
+
+	listener1, _ := config.Listen(context.Background(), network, address) // bind to the address:port
+	go listen(1, listener1)
+
+	// as soon as listener2 is bound below, all traffic will begin to flow it instead of listener1. the only thing left to do
+	// with listener1 is to shut down any active connections and close the listener.
+	listener2, _ := config.Listen(context.Background(), network, address) // also bind to the address:port
+	listen(2, listener2)
+}
+
+func listen(id int, listener net.Listener) {
+	socket, _ := listener.Accept()
+	log.Println("Accepted socket from listener", id)
+	socket.Close()
+	listener.Close()
+}
+
+// SO_REUSEPORT                     = 0xf
+//SOL_SOCKET                       = 0x1
+func reusePort(network, address string, conn syscall.RawConn) error {
+	return conn.Control(func(descriptor uintptr) {
+		syscall.SetsockoptInt(int(descriptor), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
+	})
+}
